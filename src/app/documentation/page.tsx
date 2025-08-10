5027bca8a4e6a075795d4154f783635ab2420fc5
Davi Armani <dmartinsarmani@gmail.com>
2025-08-10 03:12:47 +0000

    Certo. Agora preciso de dois botões abaixo da pasta: um pra ir pro googl

--- src/app/documentation/page.tsx
+++ src/app/documentation/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { FileText, Sheet } from "lucide-react";
+import Link from "next/link";
 
 export default function DocumentationPage() {
   const driveFolderId = "1CdrLez4Saz7_HES30UyokJGBguoAiG7D";
-  const embedUrl = `https://drive.google.com/embeddedfolderview?id=${driveFolderId}#list`;
+  const embedUrl = `https://drive.google.com/embeddedfolderview?id=${driveFolderId}#grid`;
 
   return (
     <div className="flex flex-col flex-1 p-4 md:p-8">
@@ -17,6 +20,20 @@ export default function DocumentationPage() {
           className="w-full h-full border-0"
           allow="fullscreen"
         ></iframe>
+      </div>
+      <div className="flex justify-center gap-4 mt-4">
+        <Button asChild>
+          <Link href="https://docs.google.com/document/create" target="_blank">
+            <FileText className="mr-2 h-4 w-4" />
+            Novo Documento
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="https://sheets.google.com/create" target="_blank">
+            <Sheet className="mr-2 h-4 w-4" />
+            Nova Planilha
+          </Link>
+        </Button>
       </div>
     </div>
   );
